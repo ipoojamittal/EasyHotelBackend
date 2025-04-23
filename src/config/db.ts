8dd2a256d63f213f56d7da7f536c6f29f657dfee
Pooja Mittal <official.poojamittal@gmail.com>
2025-04-24 03:28:45 +0530

    Created basic db connection method

---
+++ src/config/db.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); // Load .env variables
+const connectDB = async () => {
+    try {
+        const mongoURI = process.env.DATABASE_URL;
+        if (!mongoURI) {
+            console.error('db.ts/connectDB(): DATABASE_URL is not defined in .env file');
+            process.exit(1); // Exit process with failure
+        }
+        await mongoose.connect(mongoURI);
+        mongoose.connection.on('error', (err) => {
+            console.error(`db.ts/connectDB(): MongoDB connection error: ${err}`);
+            process.exit(1);
+        })
+        mongoose.connection.on('disconnected', () => {
+            console.log('db.ts/connectDB(): MongoDB disconnected.');
+        });
+        mongoose.connection.on('connected', () => {
+            console.log('db.ts/connectDB(): MongoDB connected.');
+        });
+    }
+    catch (error) {
+        console.error(`db.ts/connectDB(): MongoDB connection error: ${error}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
